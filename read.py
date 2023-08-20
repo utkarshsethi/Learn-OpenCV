c62fd0a84dc2879f0720804d9d49b2f3eb3c4b1e
Utkarsh Sethi <utkarsh.sethi7@gmail.com>
2023-08-20 22:45:16 +1000

    Update read.py

--- read.py
+++ read.py
@@ -2,23 +2,24 @@
 import cv2 as cv
 
 # # Read sample image
-img = cv.imread('Photos/sample1.jpg')
+img = cv.imread('Resources/Photos/cat.jpg')
+print(img)
 
 # display the image
 cv.imshow('Sample Image', img)
 
-cv.waitKey(0) # wait for key press, 0 means wait indefinitely
+cv.waitKey(0)  # wait for key press, 0 means wait indefinitely
 
 # Read video
-videoFrame = cv.VideoCapture('Videos/Sample 144p.mp4')
+# videoFrame = cv.VideoCapture('Videos/Sample 144p.mp4')
 
-while True:
-    isTrue, frame = videoFrame.read()
+# while True:
+#     isTrue, frame = videoFrame.read()
 
-    cv.imshow('Video', frame)
+#     cv.imshow('Video', frame)
 
-    if cv.waitKey(20) & 0xFF == ord('d'):   # if 'd' is pressed or 20ms passed, exit
-        break
+#     if cv.waitKey(20) & 0xFF == ord('d'):   # if 'd' is pressed or 20ms passed, exit
+#         break
 
-videoFrame.release()
-cv.destroyAllWindows()
+# videoFrame.release()
+# cv.destroyAllWindows()
